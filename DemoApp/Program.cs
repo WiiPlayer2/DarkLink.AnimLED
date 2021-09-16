ee6c46dbd6b34d247b7ae3307e4770a8a822535d
Waldemar Tomme <waldetomme@gmail.com>
2021-09-16 06:56:10 +0200

    Update DemoApp

--- DemoApp/Program.cs
+++ DemoApp/Program.cs
@@ -22,21 +22,26 @@ namespace DemoApp
 
             var metaData = new PaletteMetaData(new ColorR8G8B8[]
             {
-                new(0x00, 0x00, 0x00),
+                new(0x00, 0xFF, 0x00),
                 new(0xFF, 0x00, 0xFF),
             });
-            var imageData = new PaletteImageData(8, 8, metaData, 4, 1);
-            imageData.Frames[0][0, 0] = 1;
-            imageData.Frames[1][7, 0] = 1;
-            imageData.Frames[2][7, 7] = 1;
-            imageData.Frames[3][0, 7] = 1;
+            var imageData = new PaletteImageData(8, 8, metaData, 2, 1);
+            for (var x = 0; x < 8; x++)
+            {
+                for (var y = 0; y < 8; y++)
+                {
+                    var index = (y * 9 + x) % 2;
+                    imageData.Frames[0][x, y] = (byte)index;
+                    imageData.Frames[1][x, y] = (byte) (1 - index);
+                }
+            }
 
             var device = new Device(serialPort.BaseStream);
             await device.Call(new Update
             {
                 ColorFormat = imageData.Format,
                 Frames = (byte) imageData.Frames.Count,
-                Delay = 100,
+                Delay = 500,
                 Loop = true,
             });
             await device.SendPacket(imageData.Serialize());
